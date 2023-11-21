0cf5a3081d1a77841051989d1d82803d786456c0
Fidan Ademi <fidomax07@gmail.com>
2023-11-21 08:17:31 +0100

    Add tabs into the users list page

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +18,28 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All Users')
+                ->icon('heroicon-m-user-group')
+                ->badge(User::count())
+                ->badgeColor('info'),
+
+            'active' => Tab::make('Activated Users')
+                ->icon('heroicon-m-shield-check')
+                ->badge(User::query()->where('is_active', true)->count())
+                ->badgeColor('success')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('is_active', true)),
+
+            'inactive' => Tab::make('Inactive Users')
+                ->icon('heroicon-m-shield-exclamation')
+                ->badge(User::query()->where('is_active', false)->count())
+                ->badgeColor('danger')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('is_active', false)),
         ];
     }
 }
