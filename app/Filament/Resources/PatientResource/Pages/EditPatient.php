ca98ab4c3cbad130674016e5cf56dc2e5fbfe0b8
Fidan Ademi <fidomax07@gmail.com>
2023-12-15 07:48:23 +0100

    Customize saved notification on the EditPatient page

--- app/Filament/Resources/PatientResource/Pages/EditPatient.php
+++ app/Filament/Resources/PatientResource/Pages/EditPatient.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\PatientResource\Pages;
 
 use App\Filament\Resources\PatientResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPatient extends EditRecord
@@ -22,5 +23,13 @@ class EditPatient extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Patient updated')
+            ->body('The patient has been saved successfully.');
     }
 }
