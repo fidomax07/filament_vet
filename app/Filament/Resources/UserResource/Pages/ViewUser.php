f8e47b37b04125a1fb95a5d52bf67b2b557909ee
Fidan Ademi <fidomax07@gmail.com>
2023-12-17 17:26:08 +0100

    Customize user's view page heading

--- app/Filament/Resources/UserResource/Pages/ViewUser.php
+++ app/Filament/Resources/UserResource/Pages/ViewUser.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
+use Illuminate\Support\HtmlString;
+use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Contracts\Support\Htmlable;
+
 
 class ViewUser extends ViewRecord
 {
@@ -15,5 +19,13 @@ class ViewUser extends ViewRecord
         return [
             Actions\EditAction::make(),
         ];
+    }
+
+    public function getHeading(): string|Htmlable
+    {
+        /** @var User $user */
+        $user = $this->record;
+
+        return new HtmlString("<span class='font-normal'>Viewing:</span> $user->name");
     }
 }
