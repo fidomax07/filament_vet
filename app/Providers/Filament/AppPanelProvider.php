260698927dfc8b42777f271694e0d82c8c44d97b
Fidan Ademi <fidomax07@gmail.com>
2023-12-29 17:27:34 +0100

    Update Filament panel provider to support hot reloading with Vite

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers\Filament;
 
+use Illuminate\Support\Facades\Blade;
 use Filament\Http\Middleware\Authenticate;
+use Filament\Support\Facades\FilamentView;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
@@ -18,17 +20,18 @@ use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
-class AdminPanelProvider extends PanelProvider
+class AppPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
             ->default()
-            ->id('admin')
-            ->path('admin')
+            ->id('app')
+            ->path('')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Indigo,
+                'gray' => Color::Slate
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -58,5 +61,15 @@ class AdminPanelProvider extends PanelProvider
             // Below SPA configuration doesn't seem to currently work, will try again at some
             // later time, to see if the JS error is fixed and this is working as expected.
             //->spa();
+    }
+
+    public function register(): void
+    {
+        parent::register();
+
+        FilamentView::registerRenderHook(
+            'panels::body.end',
+            fn(): string => Blade::render("@vite('resources/js/app.js')")
+        );
     }
 }
