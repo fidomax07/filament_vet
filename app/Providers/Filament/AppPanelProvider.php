364c887e488c67a4ade80afa2658f9afc6f3a2e0
Fidan Ademi <fidomax07@gmail.com>
2024-01-01 16:06:58 +0100

    Add a custom EditProfile page

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -2,23 +2,24 @@
 
 namespace App\Providers\Filament;
 
-use Illuminate\Support\Facades\Blade;
-use Filament\Http\Middleware\Authenticate;
-use Filament\Support\Facades\FilamentView;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
+use Filament\Widgets;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Support\Facades\Blade;
+use App\Filament\Pages\Auth\EditProfile;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Support\Facades\FilamentView;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 class AppPanelProvider extends PanelProvider
 {
@@ -29,9 +30,10 @@ class AppPanelProvider extends PanelProvider
             ->id('app')
             ->path('')
             ->login()
+            ->profile(EditProfile::class)
             ->colors([
                 'primary' => Color::Indigo,
-                'gray' => Color::Slate
+                'gray' => Color::Slate,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -58,9 +60,9 @@ class AppPanelProvider extends PanelProvider
                 Authenticate::class,
             ])
             ->globalSearchKeyBindings(['command+k', 'ctrl+k']);
-            // Below SPA configuration doesn't seem to currently work, will try again at some
-            // later time, to see if the JS error is fixed and this is working as expected.
-            //->spa();
+        // Below SPA configuration doesn't seem to currently work, will try again at some
+        // later time, to see if the JS error is fixed and this is working as expected.
+        //->spa();
     }
 
     public function register(): void
@@ -69,7 +71,7 @@ class AppPanelProvider extends PanelProvider
 
         FilamentView::registerRenderHook(
             'panels::body.end',
-            fn(): string => Blade::render("@vite('resources/js/app.js')")
+            fn (): string => Blade::render("@vite('resources/js/app.js')")
         );
     }
 }
